0408e21d5b819d5a731d5b261cfffaa51c8082a3
DaddyNexus <100282003+DaddyNexus@users.noreply.github.com>
2025-11-23 03:53:10 +0800

    Update Checkpos.cs

--- Commands/Checkpos.cs
+++ Commands/Checkpos.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Logger = Rocket.Core.Logging.Logger;
 
-namespace YourPluginNamespace
+namespace nexus
 {
     public class CommandCheckPos : IRocketCommand
     {
